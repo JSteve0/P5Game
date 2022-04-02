57bf04f07243f56fc97bb1ba0c6c7e1fd28b3208
Justin Stevens <49693694+JSteve0@users.noreply.github.com>
2022-04-02 02:47:48 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+//Frostbyt3 Game
+
+//Vars
+keys = [];
+
+//Setup objects, window, and vars
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background('black');
+}
